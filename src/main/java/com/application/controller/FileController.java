77405e83a8a5d04c01639a7b343d5db43ddab3cd
George <chaoboma@gmail.com>
2025-03-19 16:11:16 +0800

    Add shapefileDataStore.dispose();

--- src/main/java/com/application/controller/FileController.java
+++ src/main/java/com/application/controller/FileController.java
@@ -426,6 +426,7 @@ public class FileController {
                 }
                 geojsonObject.put("crs", crsJson);
                 uploadShpResp.setGeojson(geojsonObject);
+                shapefileDataStore.dispose();
 
             }catch (Exception e){
                 e.printStackTrace();
