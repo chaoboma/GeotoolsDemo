e265f21a2aa89f19c17ba86ccf734f843d29bbaa
George <chaoboma@gmail.com>
2025-03-25 17:59:14 +0800

    test MappedByteBuffer in Download big file

--- src/main/java/com/application/config/TomcatConfig.java
+++ src/main/java/com/application/config/TomcatConfig.java
@@ -1,22 +1,22 @@
-package com.application.config;
-
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServerFactoryCustomizer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class TomcatConfig {
-
-    @Bean
-    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> tomcatCustomizer() {
-        return factory -> {
-            factory.addConnectorCustomizers(connector -> {
-                // 设置协议为NIO
-                connector.setProperty("protocol", "org.apache.coyote.http11.Http11NioProtocol");
-                // 设置使用直接内存
-                connector.setProperty("useDirectBuffers", "true");
-            });
-        };
-    }
-}
+//package com.application.config;
+//
+//import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+//import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class TomcatConfig {
+//
+//    @Bean
+//    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> tomcatCustomizer() {
+//        return factory -> {
+//            factory.addConnectorCustomizers(connector -> {
+//                // 设置协议为NIO
+//                connector.setProperty("protocol", "org.apache.coyote.http11.Http11NioProtocol");
+//                // 设置使用直接内存
+//                connector.setProperty("useDirectBuffers", "true");
+//            });
+//        };
+//    }
+//}
